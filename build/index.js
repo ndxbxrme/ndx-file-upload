78b3e30e90bc96de8e0a7f9a153408e96ad4d869
ndxbxrme <lewis_the_cat@hotmail.com>
2017-04-22 20:37:52 +0100

    first commit

---
+++ build/index.js
@@ -0,0 +1,77 @@
+(function() {
+  'use strict';
+  var fs, mkdirp, multiparty, path;
+
+  fs = require('fs');
+
+  path = require('path');
+
+  mkdirp = require('mkdirp');
+
+  multiparty = require('connect-multiparty');
+
+  module.exports = function(ndx) {
+    var callbacks, syncCallback;
+    callbacks = {
+      upload: []
+    };
+    syncCallback = function(name, obj, cb) {
+      var callback, i, len, ref;
+      if (callbacks[name] && callbacks[name].length) {
+        ref = callbacks[name];
+        for (i = 0, len = ref.length; i < len; i++) {
+          callback = ref[i];
+          callback(obj);
+        }
+      }
+      return typeof cb === "function" ? cb() : void 0;
+    };
+    return ndx.app.post('/api/uploads', multiparty(), function(req, res) {
+      var folder, output;
+      output = [];
+      folder = './uploads';
+      if (req.body.folder) {
+        folder = path.join(folder, req.body.folder);
+      }
+      return mkdirp(folder, function(err) {
+        var file, i, len, ref, saveFile;
+        saveFile = function(file) {
+          var filename, outobj, outpath, rs, ws;
+          filename = ndx.generateID(12) + path.extname(file.originalFilename);
+          outpath = path.join(folder, filename);
+          rs = fs.createReadStream(file.path);
+          ws = fs.createWriteStream(outpath);
+          rs.pipe(ws);
+          rs.on('end', function() {
+            return fs.unlinkSync(file.path);
+          });
+          outobj = {
+            filename: filename,
+            path: outpath,
+            originalFilename: file.originalFilename,
+            type: file.type,
+            size: file.size,
+            date: new Date().valueOf(),
+            ext: path.extname(file.originalFilename).replace(/^\./, '')
+          };
+          ndx.extend(outobj, req.body);
+          syncCallback('upload', outobj);
+          return outobj;
+        };
+        if (Object.prototype.toString.call(req.files.file) === '[object Array]') {
+          ref = req.files.file;
+          for (i = 0, len = ref.length; i < len; i++) {
+            file = ref[i];
+            output.push(saveFile(file));
+          }
+        } else {
+          output.push(saveFile(req.files.file));
+        }
+        return res.json(output);
+      });
+    });
+  };
+
+}).call(this);
+
+//# sourceMappingURL=index.js.map
---
+++ src/index.coffee
@@ -0,0 +1,45 @@
+'use strict'
+fs = require 'fs'
+path = require 'path'
+mkdirp = require 'mkdirp'
+multiparty = require 'connect-multiparty'
+
+module.exports = (ndx) ->
+  callbacks =
+    upload: []
+  syncCallback = (name, obj, cb) ->
+    if callbacks[name] and callbacks[name].length
+      for callback in callbacks[name]
+        callback obj
+    cb?()
+  ndx.app.post '/api/uploads', multiparty(), (req, res) ->
+    output = []
+    folder = './uploads'
+    if req.body.folder
+      folder = path.join folder, req.body.folder
+    mkdirp folder, (err) ->
+      saveFile = (file) ->
+        filename = ndx.generateID(12) + path.extname(file.originalFilename)
+        outpath = path.join(folder, filename)
+        rs = fs.createReadStream file.path
+        ws = fs.createWriteStream outpath
+        rs.pipe ws
+        rs.on 'end', ->
+          fs.unlinkSync file.path
+        outobj =
+          filename: filename
+          path: outpath
+          originalFilename: file.originalFilename
+          type: file.type
+          size: file.size
+          date: new Date().valueOf()
+          ext: path.extname(file.originalFilename).replace /^\./, ''
+        ndx.extend outobj, req.body
+        syncCallback 'upload', outobj
+        outobj
+      if Object.prototype.toString.call(req.files.file) is '[object Array]'
+        for file in req.files.file
+          output.push saveFile file
+      else
+        output.push saveFile req.files.file
+      res.json output
